19133b30aa9afe988fc0d32ce8addbde7ec0a5ff
demurelian <demurelian@gmail.com>
2024-04-03 11:19:10 +0300

    Добавили статус программы

--- PRNcompression/ViewModels/MainWindowViewModel.cs
+++ PRNcompression/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,12 @@ namespace PRNcompression.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-
+        private string _ProgramStatus = "Status is OK";
+        public string ProgramStatus
+        {
+            get => _ProgramStatus;
+            set => Set(ref _ProgramStatus, value);
+        }
 
         public MainWindowViewModel() 
         {
